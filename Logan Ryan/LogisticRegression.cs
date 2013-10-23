737431658debcfb14ad4ecfb83015fb4e6aec1de
ryanlf <ryanlf@rose-hulman.edu>
2013-10-22 20:36:12 -0400

    Added my code

---
+++ Logan Ryan/LogisticRegression.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra.Single;
+
+
+namespace WatsonCompetitionCode
+{
+    class LogisiticRegression
+    {
+        private Dictionary<int, Canidate> trainingDataSet;
+        private DenseVector theta;
+        private DenseVector J;
+        private DenseMatrix x;
+        private DenseVector y;
+        private DenseMatrix hessianResult;
+        public const int MAX_ITERATIONS= 7;
+        private int m;
+        private int n;
+        
+
+
+        public LogisiticRegression(Dictionary<int,Canidate> data)
+        {
+            this.trainingDataSet = data;
+            int numberFeatures = trainingDataSet[1].featuresRating.Count;
+
+            //Initialize xdata matrix
+            DenseVector[] xdata;
+            xdata = new DenseVector[trainingDataSet.Count];
+            //xdata[0] = new DenseVector(numberFeatures,1); 
+            int k = 0;
+           
+
+            //intialize y data
+            List<float> ydata = new List<float>();
+            
+
+            //fill x and y data from dictionary
+            foreach (KeyValuePair<int,Canidate> canidate in trainingDataSet)
+            {
+                List<float> intermediate = canidate.Value.featuresRating.ToList();
+                intermediate.Insert(0, 1);
+                xdata[k] = new DenseVector(intermediate.ToArray());
+                if (canidate.Value.isTrue) ydata.Add(1);
+                else ydata.Add(0);
+                k++;
+            }
+
+            //populate fields with data
+            x = DenseMatrix.OfRowVectors(xdata);
+            y = new DenseVector(ydata.ToArray());
+
+            m = x.RowCount;
+            n = x.ColumnCount-1;
+
+            //Intialize fitting parameters, theta
+            theta = new DenseVector(m+1,1);
+            /*test code
+            DenseVector z;
+            z = x * theta;
+            float[] test1;
+            test1 = new float[3] {2,3,4};
+            float[] test2;
+            test2 = new float[3] {4,3,2};
+            DenseVector test1V = new DenseVector(test1);
+            DenseVector test2V = new DenseVector(test2);
+            DenseVector result = (DenseVector) test1V.PointwiseMultiply(test2V);*/
+            
+
+
+            //Console.ReadLine();
+        }
+
+        private DenseVector sigmoid(DenseVector z)
+        {
+            List<float> vector = new List<float>();
+            foreach (float element in z)
+            {
+                vector.Add((float)(1.0 / (1.0 + Math.Exp(-element))));
+            }
+            return new DenseVector(vector.ToArray());
+        }
+        public float probability(List<float> features)
+        {
+            return 0;
+        }
+        private DenseVector gradient(DenseMatrix xMat, DenseVector hVec, DenseVector yVec)
+        {
+            DenseVector grad = (DenseVector)(xMat.Transpose().Divide(m) * (hVec - yVec));
+            return grad;
+        }
+        private DenseMatrix hessian(DenseMatrix xMat, DenseVector hVec)
+        {
+            DenseMatrix result;
+            DiagonalMatrix diag = DiagonalMatrix.Identity(hVec.Count);
+            diag.SetDiagonal(hVec);
+            DiagonalMatrix diagMinus1 = DiagonalMatrix.Identity(hVec.Count);
+            diagMinus1.SetDiagonal(1-hVec);
+            result = (DenseMatrix) ((xMat.Transpose().Divide(m)) * diag * diagMinus1 * xMat);
+            return result;
+        }
+        public void train(int iterations = MAX_ITERATIONS)
+        {
+            J = new DenseVector(iterations, 0);
+            for (int k = 0; k < iterations; k++)
+            {
+                DenseVector z = x * theta;
+                DenseVector h = sigmoid(z);
+                DenseVector grad = gradient(x, h, y);
+                DenseMatrix H = hessian(x, h);
+
+                //Calculate J for testing convergence
+                J[k] = (float) (y.Negate().PointwiseMultiply((DenseVector) log(h)) - (1-y).PointwiseMultiply( (DenseVector) log((DenseVector)(1-h)))).Sum();
+                theta = (DenseVector) (theta - H.Inverse()*grad);
+            }
+        }
+        private DenseVector log(DenseVector vec)
+        {
+            List<float> result = new List<float>();
+            foreach (float element in vec)
+            {
+                result.Add((float)Math.Log(element));
+            }
+            return new DenseVector(result.ToArray());
+        }
+        
+    }
+    
+}
+
---
+++ Logan Ryan/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace WatsonCompetitionCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Util utility = new Util();
+            Dictionary<int, Canidate> dict = utility.csvReader("TGMC training-sample.csv");
+            //utility.fileWriter(dict,"test.txt");
+            //Decision Trees need to be before this preprocessor.
+            utility.removeExtraneouData(dict);
+            
+            LogisiticRegression ai = new LogisiticRegression(dict);
+            ai.train();
+
+            Console.ReadLine();
+        }
+
+       
+    }
+    class Canidate
+    {
+        public int rowNumber;
+        public float questionNumber;
+        public List<float> featuresRating;
+        public Boolean isTrue;
+        public Boolean givenAnswer;
+
+        public Canidate()
+        {
+            rowNumber = 0;
+            questionNumber = 0;
+            featuresRating = new List<float>();
+            isTrue = false;
+            givenAnswer = false;
+        }
+    }
+    class Util 
+    {
+         public Dictionary<int,Canidate> csvReader(string fileName)
+        {
+            var reader = new StreamReader(File.OpenRead(@fileName));
+            List<string> lineRead = new List<string>();
+            Dictionary<int,Canidate> canidates = new Dictionary<int,Canidate>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                Canidate canidate = new Canidate();
+                canidate.rowNumber = Convert.ToInt32(values[0]);
+                canidate.questionNumber = Convert.ToSingle(values[1]);
+                for (int i = 2; i < values.Count(); i++)
+                {
+                    if (values[i] == "true" || values[i] == "false" || values[i] == "TRUE" || values[i] == "FALSE")
+                    {
+                        if (values[i] == "true" || values[i] == "TRUE") canidate.isTrue = true;
+                        else canidate.isTrue = false;
+                        canidate.givenAnswer = true;
+                        break;
+                    }
+                    canidate.featuresRating.Add(Convert.ToSingle(values[i]));
+                }
+                canidates.Add(canidate.rowNumber, canidate);
+            }
+            return canidates;
+        }
+        public void fileWriter(Dictionary<int, Canidate> canidates,string fileName)
+        {
+            var writer = new StreamWriter(fileName);
+            int i = 1;
+            Canidate canidate = new Canidate();
+            while (canidates.ContainsKey(i))
+            {
+                canidates.TryGetValue(i, out canidate);
+                if (canidate.isTrue)
+                {
+                    writer.WriteLine(canidate.questionNumber.ToString());
+                }
+                i++;
+            }
+            writer.Close();
+        }
+        public List<int> removeExtraneouData(Dictionary<int, Canidate> canidates)
+        {
+            //intialize size of array of columns
+            List<List<float>> columns = new List<List<float>>();
+            int columnCount = canidates.Values.First().featuresRating.Count();
+            for (int k = 0; k < columnCount; k++)
+            {
+                columns.Add(new List<float>());
+            }
+
+            //Generate Array of Columns
+            foreach (KeyValuePair<int, Canidate> pair in canidates)
+            {
+                Canidate c = pair.Value;
+                int length = c.featuresRating.Count;
+                for (int i = 0; i < length; i++)
+                {
+                    columns[i].Add(c.featuresRating[i]);
+                }
+            }
+
+            //Add columns to remove that contain same data
+            List<int> removeData = new List<int>();
+            for (int k = 0; k < columnCount-1;k++)
+            {
+                for (int i = k + 1; i < columnCount; i++)
+                {
+                    if (columns[k].Sum() == columns[i].Sum())
+                    {
+                        bool isRemove = true;
+                        for (int j = 0; j < columns[k].Count; j++)
+                        {
+                            if (columns[k][j] != columns[i][j])
+                            {
+                                isRemove = false;
+                                break;
+                            }
+                        }
+                        if (isRemove && !removeData.Contains(i))
+                        {
+                            removeData.Add(i);
+                        }
+                    }
+                }
+            }
+            //Add columns that have all same values (without doubling up on previous addition)
+            for (int k = 0; k < columns.Count; k++)
+            {
+                float firstElement = columns[k][0];
+                bool isRemove = true;
+                foreach (float feature in columns[k])
+                {
+                    if (firstElement != feature)
+                    {
+                        isRemove = false;
+                        break;
+                    }
+                }
+                if (isRemove && !removeData.Contains(k))
+                {
+                    removeData.Add(k);
+                }
+            }
+
+            List<int> result = removeData.ToList();
+
+            while (removeData.Count >0)
+            {
+                foreach (KeyValuePair<int, Canidate> pair in canidates)
+                {
+                    pair.Value.featuresRating.RemoveAt(removeData.Max());
+                }
+                columns.RemoveAt(removeData.Max());
+                //Console.WriteLine("Removed Column " + removeData.Max());
+                removeData.Remove(removeData.Max());
+            }
+            List<float> maxValues = new List<float>();
+            foreach (List<float> column in columns)
+            {
+                float absMax;
+                if (Math.Abs(column.Min()) > Math.Abs(column.Max())) absMax = Math.Abs(column.Min());
+                else absMax = Math.Abs(column.Max());
+                if (absMax == 0.0) absMax = (float) 1.0;
+                maxValues.Add(absMax);
+            }
+
+            for (int k = 0; k < maxValues.Count; k++)
+            {
+                foreach (KeyValuePair<int, Canidate> canidate in canidates)
+                {
+                    canidate.Value.featuresRating[k] = (canidate.Value.featuresRating[k] / maxValues[k]);
+                }
+            }
+            return result;
+        }
+    }
+}
